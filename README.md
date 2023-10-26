6b18a38c9a939c1c6e3087956d5e73ca02d34329
adelhpour <>
2023-10-26 16:29:31 -0700

    First version of the project is created.

---
+++ README.md
@@ -0,0 +1,5 @@
+# RoadRunnerLoopDetect
+
+A python script to find the loops presented in the Jacobin matrix of Roadrunner using LoopDetect.
+
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from setuptools import find_packages
+
+MAJOR = 0
+MINOR = 0
+MICRO = 0
+
+version = f'{MAJOR}.{MINOR}.{MICRO}'
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="roadrunnerloopdetect",
+    version=version,
+    author="Adel Heydarabadipour",
+    author_email="adelhp@uw.edu",
+    description="Find the loops presented in the Jacobin matrix of Roadrunner using LoopDetect",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adelhpour/RoadRunnerLoopDetect",
+    project_urls={
+        "Bug Tracker": "https://github.com/adelhpour/RoadRunnerLoopDetect/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["roadrunner", "loopdetect"],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.8"
+)
