222fdb2869b0742d5d7a38bc75bf931751215802
adelhpour <>
2023-10-31 23:39:22 -0700

    syntax refactor

--- src/sbmlloopdetect/sbmlloopdetect.py
+++ src/sbmlloopdetect/sbmlloopdetect.py
@@ -6,8 +6,7 @@ import loopdetect.core
 
 def detect(sbml):
     rr = None
-    antimony.clearPreviousLoads()
-    antimony.freeAll()
+    initialize_antimony()
     is_file = check_if_it_is_file(sbml)
     if is_file:
         rr = get_roadrunner_object_from_file(sbml)
@@ -16,11 +15,18 @@ def detect(sbml):
 
     return loopdetect.core.find_loops_noscc(rr.getFullJacobian())
 
+
+def initialize_antimony():
+    antimony.clearPreviousLoads()
+    antimony.freeAll()
+
+
 def check_if_it_is_file(possible_file):
     if os.path.isfile(possible_file):
         return True
 
     return False
+
 
 def get_roadrunner_object_from_file(file):
     code = antimony.loadAntimonyFile(file)
@@ -28,6 +34,7 @@ def get_roadrunner_object_from_file(file):
         return roadrunner.RoadRunner(antimony.getSBMLString())
     else:
         return roadrunner.RoadRunner(file)
+
 
 def get_roadrunner_object_from_string(string):
     code = antimony.loadAntimonyString(string)
