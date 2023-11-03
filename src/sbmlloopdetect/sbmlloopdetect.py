269be5164793e74e353e32d40f73aea9d86435cb
adelhpour <>
2023-11-03 01:47:47 -0700

    it is now possible for the user to exclude the loop legnths and loop signs from the loop list by providing the detect function with some arguments

--- src/sbmlloopdetect/sbmlloopdetect.py
+++ src/sbmlloopdetect/sbmlloopdetect.py
@@ -2,9 +2,10 @@ import os
 import roadrunner
 import antimony
 import loopdetect.core
+import pandas
 
 
-def detect(sbml):
+def detect(sbml, filter_loop_length_list=[], filter_loop_sign=None):
     rr = None
     initialize_antimony()
     is_file = check_if_it_is_file(sbml)
@@ -13,7 +14,13 @@ def detect(sbml):
     else:
         rr = get_roadrunner_object_from_string(sbml)
 
-    return loopdetect.core.find_loops_noscc(rr.getFullJacobian())
+    loop_list = loopdetect.core.find_loops_noscc(rr.getFullJacobian())
+    if len(filter_loop_length_list):
+        loop_list = filter_length(loop_list, filter_loop_length_list)
+    if filter_loop_sign:
+        loop_list = filter_sign(loop_list, filter_loop_sign)
+
+    return loop_list
 
 
 def initialize_antimony():
@@ -42,3 +49,9 @@ def get_roadrunner_object_from_string(string):
         return roadrunner.RoadRunner(antimony.getSBMLString())
     else:
         return roadrunner.RoadRunner(string)
+
+def filter_length(loop_list, loop_length_list):
+    return loop_list[~loop_list['length'].isin(loop_length_list)]
+
+def filter_sign(loop_list, loop_sign):
+    return loop_list[loop_list['sign'] != loop_sign]
