eee014911b351cf5f21239f6753e2587ae25eb54
adelhpour <>
2023-11-09 17:32:07 -0800

    examples are updated according to the latest changes

--- examples/antimony_string_input.py
+++ examples/antimony_string_input.py
@@ -16,6 +16,5 @@ k1 = 0.1 # The value of the kinetic parameter from J0.
 k2 = 0.2 # The value of the kinetic parameter from J1.
 """
 
-
-loop_list = sbmlloopdetect.detect(antimony_string, filter_loop_length_list=[2, 3, 4], filter_loop_sign=1, max_num_loops=10)
+loop_list = sbmlloopdetect.detect(antimony_string, filter_loop_length_list=[2, 3, 4], filter_positive_loops=True, max_num_loops=10)
 print(loop_list)
--- examples/sbml_string_input.py
+++ examples/sbml_string_input.py
@@ -60,5 +60,5 @@ sbml_string = """
 </sbml>
 """
 
-loop_list = sbmlloopdetect.detect(sbml_string, filter_loop_length_list=[2, 3, 4], filter_loop_sign=1, max_num_loops=10)
+loop_list = sbmlloopdetect.detect(sbml_string, filter_loop_length_list=[2, 3, 4], filter_positive_loops=True, max_num_loops=10)
 print(loop_list)
